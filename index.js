4db151cb4bdf1ff75b43314d91fc75395c3e7329
kumarivatsala <60508980+kumarivatsala@users.noreply.github.com>
2020-02-11 02:20:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>MY FIRST WEBPAGE</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <table cellspacing="20">
+    <tr>
+      <td><img src="main.png" alt="MY SELFIE" width="500" height="300"></td>
+      <td><h1>SAKET KASHYAP</h1>
+      <p><em>Student of <strong>SRMIST</strong></em></p>
+      <p>I am a <a href="https://en.wikipedia.org/wiki/Bachelor_of_Technology">b.tech</a> student currently studying in 2nd year. I love tea. I am in <a href="https://en.wikipedia.org/wiki/Chennai">chennai</a> right now.</p></td>
+    </tr>
+  </table>
+  <hr>
+  <h3>EDUCATION</h3>
+  <ul>
+    <li>
+      ST. XAVIER'S HIGH SCHOOL
+    </li>
+    <li>
+      <a href="http://leedsasianschool.com/">LEEDS ASIAN SCHOOL</a>
+    </li>
+    <li>
+      <a href="https://www.srmist.edu.in/">SRMIST</a>
+    </li>
+  </ul>
+  <hr>
+  <h3>HOOBIES</h3>
+  <ol>
+    <li>
+      <a href="https://www.youtube.com/watch?v=g-beFHld19c">CRICKET</a>
+    </li>
+    <LI>
+      READING BOOKS
+    </LI>
+    <LI>
+      RUNNING
+    </LI>
+  </ol>
+  <hr>
+  <h3>YEARS</h3>
+  <table border="1">
+    <thead>
+      <tr>
+        <td>INSTITUTATION</td>
+        <td>YEARS</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>ST. XAVIER'S HIGH SCHOOL</td>
+        <td>2005-2016</td>
+      </tr>
+      <tr>
+        <td>LEEDS ASIAN SCHOOL</td>
+        <td>2016-2018</td>
+      </tr>
+      <tr>
+        <td>SRMIST</td>
+        <td>2018-2022</td>
+      </tr>
+    </tbody>
+  </table>
+  <hr>
+  <h3>SKILLS</h3>
+        <table cellspacing="10">
+        <tr>
+          <td>C language</td>
+          <td>⭐⭐⭐⭐</td>
+          <td>HTML</td>
+          <td>⭐⭐⭐⭐</td>
+        </tr>
+        <tr>
+          <td>C++ language</td>
+          <td>⭐⭐⭐⭐</td>
+          <td>CSS</td>
+          <td>⭐⭐⭐</td>
+        </tr>
+        <tr>
+          <td>Painting</td>
+          <td>⭐</td>
+        </tr>
+      </table>
+
+      <p><strong>click <a href="https://progate.com/certificates/89154c36q4uwyp">here</a> to view my latest certificates!!!</strong></p>
+  <hr>
+  <h3>Questions</h3>
+  <form>
+    <label>Enter name : </label>
+    <input type="text" name="" value="">
+    <br>
+    <br>
+    <label>Enter question : </label>
+    <textarea name="name" rows="8" cols="80"></textarea>
+    <br>
+    <br>
+    <center>
+          <input type="submit" name="">
+    </center>
+    <input type="date">
+    <br>
+    <br>
+    <input type="file">
+  </form>
+  <hr>
+  <div class="div">
+    <a href="contact.html">CONTACT ME</a>
+  </div>
+</body>
+
+</html>
